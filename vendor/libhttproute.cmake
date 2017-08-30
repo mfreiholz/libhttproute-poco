0fa1243726f0dfe7b9ae62b4637a68b27a3bf186
mfreiholz <mfreiholz@gmail.com>
2017-08-30 09:14:33 +0200

    Build deps from source as external projects.

---
+++ vendor/libhttproute.cmake
@@ -0,0 +1,28 @@
+set(LIBHTTPROUTE_PREFIX libhttproute)
+
+ExternalProject_Add(
+	${LIBHTTPROUTE_PREFIX}
+	PREFIX ${LIBHTTPROUTE_PREFIX}
+
+	GIT_REPOSITORY https://github.com/mfreiholz/libhttproute-cpp.git
+
+	CMAKE_ARGS -DBuildShared=OFF -DBuildTests=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+
+	INSTALL_COMMAND ""
+)
+
+
+ExternalProject_Get_Property(${LIBHTTPROUTE_PREFIX} SOURCE_DIR)
+ExternalProject_Get_Property(${LIBHTTPROUTE_PREFIX} BINARY_DIR)
+message(STATUS "${LIBHTTPROUTE_PREFIX} source directory: ${SOURCE_DIR}")
+message(STATUS "${LIBHTTPROUTE_PREFIX} binary directory: ${BINARY_DIR}")
+
+
+set(LIBHTTPROUTE_INCLUDE_DIRS ${SOURCE_DIR})
+include_directories(${LIBHTTPROUTE_INCLUDE_DIRS})
+
+set(LIBHTTPROUTE_LIBRARY_DIRS ${BINARY_DIR})
+link_directories(${LIBHTTPROUTE_LIBRARY_DIRS})
+
+set(LIBHTTPROUTE_LIBS libhttproute.lib)
+set(LIBHTTPROUTE_LIBRARY libhttproute.lib)
---
+++ vendor/poco.cmake
@@ -0,0 +1,60 @@
+set(POCO_PREFIX pocoproject)
+
+if(WIN32)
+	# Windows
+	set(POCO_URL https://pocoproject.org/releases/poco-1.7.8/poco-1.7.8p3.zip)
+	# Debug/Release
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		set(POCO_BUILD_COMMAND buildwin.cmd 140 build shared debug Win32 nosamples notests msbuild)
+	else()
+		set(POCO_BUILD_COMMAND buildwin.cmd 140 build shared release Win32 nosamples notests msbuild)
+	endif()
+else(WIN32)
+	#Linux
+	set(POCO_URL https://pocoproject.org/releases/poco-1.7.8/poco-1.7.8p3.tar.gz)
+	set(POCO_BUILD_COMMAND "")
+endif(WIN32)
+
+if(WIN32)
+	ExternalProject_Add(
+		${POCO_PREFIX}
+		PREFIX ${POCO_PREFIX}
+
+		# Download
+		URL ${POCO_URL}
+
+		# Update
+		UPDATE_COMMAND ""
+
+		# Configure
+		CONFIGURE_COMMAND ""
+
+		# Build
+		BUILD_IN_SOURCE 1
+		BUILD_COMMAND ${POCO_BUILD_COMMAND}
+
+		# Install
+		INSTALL_COMMAND ""
+	)
+
+	ExternalProject_Get_Property(${POCO_PREFIX} SOURCE_DIR)
+	ExternalProject_Get_Property(${POCO_PREFIX} BINARY_DIR)
+	message(STATUS "${POCO_PREFIX} source directory: ${SOURCE_DIR}")
+	message(STATUS "${POCO_PREFIX} binary directory: ${BINARY_DIR}")
+
+	set(POCO_INCLUDE_DIRS
+		${SOURCE_DIR}/CppUnit/include
+		${SOURCE_DIR}/Foundation/include
+		${SOURCE_DIR}/JSON/include
+		${SOURCE_DIR}/Net/include
+		${SOURCE_DIR}/Util/include
+		${SOURCE_DIR}/XML/include
+	)
+	include_directories(${POCO_INCLUDE_DIRS})
+
+	set(POCO_LIBS
+		debug CppUnitd.lib PocoFoundationd.lib PocoJSONd.lib PocoNetd.lib PocoUtild.lib PocoXMLd.lib
+		optimized CppUnit.lib PocoFoundation.lib PocoJSON.lib PocoNet.lib PocoUtil.lib PocoXML.lib
+	)
+	link_directories(${SOURCE_DIR}/lib)
+endif(WIN32)
